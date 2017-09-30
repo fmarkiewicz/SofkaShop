f12cb688c71d8ad0fc2a3ddd4bad27cfe08f6486
Fryderyk Markiewicz <markiewiczfryderyk@gmail.com>
2017-10-01 00:52:20 +0200

    biblioteki do vendora, karuzela z ostatnio dodanymi produktami

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,13 @@
 module ApplicationHelper
   def login_link
     if current_user
-      link_to "Wyloguj", destroy_user_session_path, method: "delete", class: "sf-block"
+      link_to "Wyloguj", destroy_user_session_path, method: "delete"
     else
-      link_to "Zaloguj", new_user_session_path, class: "sf-block"
+      link_to "Zaloguj", new_user_session_path
     end
   end
+
+  def price_in_float(price_in_int)
+    sprintf('%.2f zł', price_in_int/100)
+  end  
 end
