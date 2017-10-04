03f3dd5efdb19a9c25b0166948e1cda71c7f3e90
Fryderyk Markiewicz <markiewiczfryderyk@gmail.com>
2017-10-04 11:26:22 +0200

    wymagane logowanie przy akcjach edytujacych obiekty

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :require_login, only: [:new, :create, :edit, :destroy]
 
   def index
     render 'static_pages/home'
   end
+
+  def require_login
+    unless user_signed_in?
+      redirect_to new_user_session_url
+    end  
+  end   
 end
