95dd7f06d85350a4594370e74f266d692d3de78a
Fryderyk Markiewicz <markiewiczfryderyk@gmail.com>
2018-01-28 11:35:28 +0100

    commenting extend in admin product

--- app/admin/product.rb
+++ app/admin/product.rb
@@ -1,5 +1,5 @@
 ActiveAdmin.register Product do
-  extend Modules::Structures
+ # extend Modules::Structures
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
