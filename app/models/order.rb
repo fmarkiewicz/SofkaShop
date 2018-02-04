be0f17e2c61875d901be574879d894c71b1c6d34
Fryderyk Markiewicz <markiewiczfryderyk@gmail.com>
2018-02-04 15:37:02 +0100

    Update order.rb

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,5 +1,5 @@
 class Order < ApplicationRecord
-  # has_and_belongs_to_many :products
+
   validates_presence_of :first_name, :last_name, :phone, :address, :city,
                         :zip_code, :email, :payment_type
   belongs_to :product
