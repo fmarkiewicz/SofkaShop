90f70013d26f9d94929a871070201ff187a5192a
Fryderyk Markiewicz <markiewiczfryderyk@gmail.com>
2018-02-07 00:55:11 +0100

    Update product.rb

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,9 +1,12 @@
 class Product < ApplicationRecord
   # belongs_to :order
   max_paginates_per 10
-  has_attached_file :product_image, styles: {thumb: "100x100>", medium: "300x300>"}, default_url: "/images/:style/missing.png"
-  validates_attachment_content_type :product_image, content_type: /^image\/(jpg|jpeg|pjpeg|png|x-png|gif)$/,
-                          message: I18n.t('paperclip.not_allowed')
+  has_attached_file :product_image, 
+                     styles: {thumb: "100x100>", medium: "300x300>"},
+                     default_url: "/images/:style/missing.png"
+  validates_attachment_content_type :product_image, 
+                                     content_type: /^image\/(jpg|jpeg|pjpeg|png|x-png|gif)$/,
+                                     message: I18n.t('paperclip.not_allowed')
 
 
   CATEGORIES = {
